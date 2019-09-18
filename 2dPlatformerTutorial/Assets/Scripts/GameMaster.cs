5d22f4110e2f4541f4ab8b371a9d8aabb0d1478a
Ville <ville_huovila@hotmail.com>
2019-09-18 18:00:06 +0300

    first commit

---
+++ 2dPlatformerTutorial/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+
+    public static GameMaster gm;
+
+    void Awake()
+    {
+        if (gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+    }
+
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public int spawnDelay = 2;
+    public Transform spawnPrefab;
+
+    public CameraShake cameraShake;
+
+    void Start()
+    {
+        if (cameraShake == null)
+        {
+            Debug.LogError("No camera shake referenced in GameMaster");
+        }
+    }
+
+    
+
+    public IEnumerator _RespawnPlayer()
+    {
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(spawnDelay);
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        Transform clone = Instantiate(spawnPrefab, spawnPoint.position, spawnPoint.rotation) as Transform;
+        Destroy(clone.gameObject, 3f);
+    }
+
+    public static void KillPlayer(Player player)
+    {
+        Destroy(player.gameObject);
+        gm.StartCoroutine(gm._RespawnPlayer());
+    }
+
+    public static void KillEnemy(Enemy enemy)
+    {
+        gm._KillEnemy(enemy);
+    }
+
+    public void _KillEnemy(Enemy _enemy)
+    {
+        GameObject _clone = Instantiate(_enemy.deathParticles, _enemy.transform.position, Quaternion.identity).gameObject;
+        Destroy(_clone, 5f);
+        cameraShake.Shake(_enemy.shakeAmt, _enemy.shakeLength);
+        Destroy(_enemy.gameObject);
+    }
+
+}
---
+++ 2dPlatformerTutorial/Assets/Scripts/Parallaxing.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallaxing : MonoBehaviour
+{
+
+
+    public Transform[] backgrounds;     // Array (list) of all the back- and foregrounds to be parallaxed
+    private float[] parallaxScales;     // The proportion of the cameras movement to move the backgrounds by
+    public float smoothing = 1f;        // How smooth the parallax is going to be. Make sure to set this above 0
+    private Transform cam;              // reference to the main cameras transform
+    private Vector3 previousCamPos;     // the position of the care in the previous frame      
+
+    void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        previousCamPos = cam.position;
+        parallaxScales = new float[backgrounds.Length];
+
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].position.z * -1;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // for each background
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+            
+        }
+
+        previousCamPos = cam.position;
+    }
+}
